9963ed417f9587fec53bda984fd704d54f3d931d
Iswarnim <93699292+Iswarnim@users.noreply.github.com>
2026-02-23 00:42:17 +0530

    Create WebController.java

---
+++ src/main/java/com/bdfb/empmark/controller/WebController.java
@@ -0,0 +1,13 @@
+package com.bdfb.empmark.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+    @GetMapping("/")
+    public String index() {
+        // This tells Spring to look for index.html in the static folder
+        return "forward:/index.html";
+    }
+}
